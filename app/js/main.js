1e882e35531516bfc34e9e1f3f093b511549bc27
ValentinaFedosieieva <valentina.fedoseieva@gmail.com>
2022-02-05 16:57:10 +0200

    added catalog

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,7 @@
 $(function(){
-
+  $('.catalog__btn').on('click', function () {
+    $('.catalog__content').toggleClass('catalog__content--active');
+    $('.catalog__btn').toggleClass('catalog__btn--active');
+  });
 });
+
