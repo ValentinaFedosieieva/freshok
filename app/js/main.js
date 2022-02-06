04c18a536cd240b6d3d8a73e453682f19e16b195
ValentinaFedosieieva <valentina.fedoseieva@gmail.com>
2022-02-06 23:12:15 +0200

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,12 @@
 $(function(){
+  $('.top-slider__inner').slick({
+    prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="../images/icons/arrow-left.svg" alt="arrow left"></button>',
+    nextArrow: '<button type="button" class="slick-btn slick-next"><img src="../images/icons/arrow-right.svg" alt="arrow right"></button>'
+
+
+  });
+
+
   $('.catalog__btn').on('click', function () {
     $('.catalog__content').toggleClass('catalog__content--active');
     $('.catalog__btn').toggleClass('catalog__btn--active');
